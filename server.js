e14b2e15120a38ab8668376e9f27b4748e0de6f8
poosh316 <colsonmerrett@gmail.com>
2026-04-10 11:38:40 -0400

    make website
    this adds the server, logs, and routes making the basis of the website

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+//imports
+const express = require('express'); //express is the way that we are getting adn using user requests
+const app = express(); //the app is to hold an express object
+const path = require('path'); //importing the path npm module to make sure that directories work for all OS
+const {logEvent, debugLog, actionLog} = require(path.join(__dirname, 'middleware', 'logEvents.js')); // functions from the middleware of log events for back end debuging and information
+const PORT = process.env.PORT || 3500; //sets the port as 3500
 
+
+//this section is just here for loging and debuging and stays at the top to be sure that it runs every time and logs what happened when a request comes in
+app.use((req, res, next) => {
+    actionLog(req.path);
+    next();//go to the next middleware function
+});
+
+
+app.use('/', require('./routes/root'));//relocates the user to the link relating to the url if it is a valid link
+
+//an exception case if the link is wrong it will send a 404 error
+app.get(/.*/, (req, res) => {
+    res.status(404).sendFile('./views/error.html', { root: __dirname }); // send the 404.html file to the client if the requested page is not found
+});
+//starts listening on the port
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
