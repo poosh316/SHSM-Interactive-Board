ce4b8875a9f49bc290d810b9755e55104e1abae4
poosh316 <colsonmerrett@gmail.com>
2026-04-21 13:35:30 -0400

    checkup for issues with js imports

---
+++ public/interact.mjs
@@ -0,0 +1,7 @@
+console.log("starting script");
+const fs = require('fs');
+// import * as path from "path";
+
+currentWindow = document.URL.split('/')[document.URL.split('/').length-1];
+
+console.log(path.join(__dirname,"hi"));
