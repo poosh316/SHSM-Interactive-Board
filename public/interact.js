792b8890cf06f84c95b68b6d8027a2c89d51e6e3
poosh316 <colsonmerrett@gmail.com>
2026-04-22 12:20:40 -0400

    post request from lights
    this adds a post request to the server so that we can move the data from the server to a database

---
+++ public/interact.js
@@ -0,0 +1,19 @@
+
+console.log("starting script");
+submitButton = document.getElementById("on")
+lightChoices = document.getElementById("lightChoices");
+currentWindow = document.URL.split('/')[document.URL.split('/').length-1];
+console.log(currentWindow);
+
+submitButton.addEventListener("click", () => {
+    console.log(lightChoices.children[0].children[1].value)
+    fetch("/info", {
+        method: "POST",
+        body: JSON.stringify({
+            red: lightChoices.children[0].children[1].value,
+            green: lightChoices.children[1].children[1].value,
+            blue: lightChoices.children[2].children[1].value
+        }),
+        headers: {"Content-Type" : "application/json"}
+    });
+});
--- public/interact.mjs
+++
@@ -1,7 +0,0 @@
-console.log("starting script");
-const fs = require('fs');
-// import * as path from "path";
-
-currentWindow = document.URL.split('/')[document.URL.split('/').length-1];
-
-console.log(path.join(__dirname,"hi"));
