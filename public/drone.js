264febda57f2f589bec983aa93e33904188287b2
RandoTheVando <lconn1@ocdsb.ca>
2026-04-28 12:26:26 -0700

    modified:   logs/actionLog.txt 	new file:   public/drone.js 	deleted:    public/interact.js 	new file:   public/lights.js 	deleted:    public/script.js 	modified:   public/styles.css 	modified:   views/about.html 	modified:   views/home.html 	modified:   views/interactPages/drone.html 	modified:   views/interactPages/lights.html 	modified:   views/signup.html

---
+++ public/drone.js
@@ -0,0 +1,11 @@
+const drone = document.getElementById('drone');
+const droneBtn = document.getElementById('droneBtn');
+
+
+droneBtn.addEventListener("click", async () => {
+    drone.src = "/images/droneOn.gif";
+    
+    setTimeout(async () => {
+        drone.src = "/images/droneOff.png";
+    }, 10000);
+});
