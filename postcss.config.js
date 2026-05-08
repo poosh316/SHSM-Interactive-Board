c73d32a8726270de0320964ec680de0791e8587b
StrawberrySugars <jasmine.soetemans@gmail.com>
2026-05-08 13:19:56 -0400

    commit to merge_four

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 const postcss = require("postcss");
 const cssnano = require("cssnano");
 const postcssPresetEnv = require("postcss-preset-env");
-const obfuscator = require("postcss-obfuscator");
+// const obfuscator = require("postcss-obfuscator");
 
-const isObfscMode = process.env.NODE_ENV === 'obfuscation';
+// const isObfscMode = process.env.NODE_ENV === 'obfuscation';
 
 //postcss.config.js / postcss.config.cjs
 module.exports = {
@@ -14,16 +14,16 @@ module.exports = {
       preset: 'default'
     }),
     postcssPresetEnv({ }),
-    obfuscator({  //obfuscate file + minimize file
-      /* options */
-      enable: isObfscMode, // Only runs when mode is set to 'obfuscation'
-      srcPath: "./views",      // Source directory of your html/js files
-      desPath: "./out",      // Where the obfuscated files will be saved
-      extensions: ['.html', '.js'], // Files to scan for class replacements when obfuscating the css
-    //   formatJson: true     // Formats the mapping JSON for readability
-        callBack: function () {
-            process.env.NODE_ENV = "production"; // to make sure postcss-obfuscator doesn't re-run.
-        },
-    }),
+    // obfuscator({  //obfuscate file + minimize file
+    //   /* options */
+    //   enable: isObfscMode, // Only runs when mode is set to 'obfuscation'
+    //   srcPath: "./views",      // Source directory of your html/js files
+    //   desPath: "./out",      // Where the obfuscated files will be saved
+    //   extensions: ['.html', '.js'], // Files to scan for class replacements when obfuscating the css
+    // //   formatJson: true     // Formats the mapping JSON for readability
+    //     callBack: function () {
+    //         process.env.NODE_ENV = "production"; // to make sure postcss-obfuscator doesn't re-run.
+    //     },
+    // }),
   ],
 };
