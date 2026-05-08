e07fda2b58752365da215729ce63c236836279a0
StrawberrySugars <jasmine.soetemans@gmail.com>
2026-05-08 12:24:54 -0400

    obfuscator branch

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require('path'); //importing the path npm module to make sure that 
 const {logEvent, debugLog, actionLog} = require(path.join(__dirname, 'middleware', 'logEvents.js')); // functions from the middleware of log events for back end debuging and information
 const PORT = process.env.PORT || 3500; //sets the port as 3500
 
+//added myself
+// const {authenticateToken} = require(path.join(__dirname, 'controllers', 'publicController.js'));
+
 
 //this section is just here for loging and debuging and stays at the top to be sure that it runs every time and logs what happened when a request comes in
 app.use((req, res, next) => {
@@ -13,12 +16,36 @@ app.use((req, res, next) => {
     next();//go to the next middleware function
 });
 
-app.use(express.static('public')); //make files in folder public accessible 
-// //it does make the actual pages in the public folder accessible by typing them in so that needs to be changed
-
 app.use("/", require('./routes/root'));//relocates the user to the link relating to the url if it is a valid link
 
-app.use("/int", require('./routes/interactSubdir') )
+app.use("/int", require('./routes/interactSubdir'));
+
+//----------------------------
+//make files in folder public accessible 
+app.use('/', express.static(path.join(__dirname, '/public')));
+//above line needs to be commented out in order for any hope of /styles.css not working for the user
+
+app.use('/', express.static(path.join(__dirname, '/build')));
+
+//call authorization for public (user should not be allowed to see the styles.css page or any pages in the public folder, 
+// but the computer will be fine and all the stuff in the public folder will show up on the website)
+
+// //currently public.js/publicController.js is not actually being called
+// app.get('/public', authenticateToken, (req, res) => {
+//     console.log("server.js authenticated public folder");
+// //   res.json({ 
+// //     message: `Hello ${req.service.name}, here is your secure data.`,
+// //     permissions: req.service.permissions 
+// //   });
+//     sendFile('./public');
+// });
+
+// app.use('/', authenticateToken, express.static(path.join(__dirname, '/public')));
+
+
+
+//----------------------------
+
 //an exception case if the link is wrong it will send a 404 error
 app.get(/.*/, (req, res) => {
     res.status(404).sendFile('./views/error.html', { root: __dirname }); // send the 404.html file to the client if the requested page is not found
