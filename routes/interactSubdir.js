f05fb6e7ce116127b9ab687442b2ce5c46ed57f5
StrawberrySugars <jasmine.soetemans@gmail.com>
2026-05-31 13:03:23 -0400

    working timer for interact pages

--- routes/interactSubdir.js
+++ routes/interactSubdir.js
@@ -3,12 +3,44 @@ const router = express.Router();
 const path = require('path');
 
 //temporary file and file path to be serverd
-router.get(/^\/$|^\/drone(\/)?$|^\/drone(.html)?$/, (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "views", "interactPages", "drone.html"));
+router.get(/^\/$|^\/drone(\/)?$|^\/drone(.ejs)?$/, (req, res) => {
+    const token = req.cookies.guest_token;
+        
+    if (token) {
+        // Decode base64 JWT payload
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const payload = JSON.parse(Buffer.from(base64, 'base64').toString('utf8'));
+        
+        // Send time remaining in seconds to EJS template
+        const currentTime = Math.floor(Date.now() / 1000);
+        const secondsRemaining = payload.exp - currentTime;
+
+        res.render(path.join(__dirname, "..", "views", "interactPages", "drone.ejs"), { secondsRemaining });
+    } else {
+        // res.redirect('error.html');
+        res.render(path.join(__dirname, "..", "views", "interactPages", "drone.ejs"), { secondsRemaining: 0 });
+    }
 });
 
-router.get(/^\/lights(\/)?(.html)?$/, (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "views", "interactPages", "lights.html"));
+router.get(/^\/lights(\/)?(.ejs)?$/, (req, res) => {
+    const token = req.cookies.guest_token;
+        
+    if (token) {
+        // Decode base64 JWT payload
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const payload = JSON.parse(Buffer.from(base64, 'base64').toString('utf8'));
+        
+        // Send time remaining in seconds to EJS template
+        const currentTime = Math.floor(Date.now() / 1000);
+        const secondsRemaining = payload.exp - currentTime;
+
+        res.render(path.join(__dirname, "..", "views", "interactPages", "lights.ejs"), { secondsRemaining });
+    } else {
+        // res.redirect('error.html');
+        res.render(path.join(__dirname, "..", "views", "interactPages", "lights.ejs"), { secondsRemaining: 0 });
+    }
 });
 
 module.exports = router;
