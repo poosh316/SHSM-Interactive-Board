5de5dd0528174483ebe9dfedc9eb55501a57bb93
poosh316 <colsonmerrett@gmail.com>
2026-06-01 13:48:53 -0400

    added spam proof
    stores local ips for 0.3 seconds after they make a request to change a value

--- server.js
+++ server.js
@@ -7,14 +7,25 @@ const PORT = process.env.PORT || 3500; //sets the port as 3500
 const mysql = require('mysql2');
 const { worker } = require('cluster');
 const pool = require(path.join(__dirname, 'middleware', 'makeDataBase.js'));
-
+const spamTime = 300;
 const session = require('express-session');
-require('dotenv').config(); 
+require('dotenv').config();
 
+var userIps = [];
 
+const piIp = "10.191.28.102";
+var currentCode = 4321;
+var prevCode = 1234;
+
+var piUsername = "razPi";
+var piPassword = "43%SureThisIsPassword";
+
+app.enable('trust proxy');
 // console.log(con.query("SELECT * FROM mytable"));
 //this section is just here for loging and debuging and stays at the top to be sure that it runs every time and logs what happened when a request comes in
 app.use((req, res, next) => {
+    // console.log(req.ip);
+    
     // console.log("\"" + req.path + "\" to: " + "actionLog");
     if (/^\/info/.test(req.path) == false) {
         actionLog(req.path);
@@ -28,14 +39,14 @@ app.use((req, res, next) => {
 
 //session setup goes before routes
 app.use(session({
-  secret: 'process.env.SESSION_SECRET',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { 
-    maxAge: 600000, //10 minutes in milliseconds
-    httpOnly: true,  
-    sameSite: 'lax'   
-} 
+    secret: 'process.env.SESSION_SECRET',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 600000, //10 minutes in milliseconds
+        httpOnly: true,
+        sameSite: 'lax'
+    }
 }));
 
 
@@ -75,58 +86,87 @@ app.get(/^\/info/, async (req, res) => {
     // res.sendStatus(204);
 });
 
+app.get(/^\/code/, async (req, res, next) => {
+    if(!req.header["piname"]||!req.header["pikey"]){
+        logEvent(`someone with ip: ${req.ip} just used /code without ${!req.header["piname"] && !req.header["pikey"] ? "username and password" : !req.header["pikey"] ? "password":"username"}`, "codeWarning");
+        next();
+    }else{
+        if(req.header["piname"] == piUsername && req.header["pikey"] == piPassword){
+            if(req.ip != piIp){
+                logEvent(`someone with ip: ${req.ip} just sussesfuly signed into code with the wrong ip`, "codeWarning");
+            }
+            res.json({QR : currentCode});
+        }else{
+            logEvent(`someone with ip: ${req.ip} just used /code without correct ${req.header["piname"] != piUsername && !req.header["pikey"] != piPassword ? `username:${req.header["piname"]} and password:${"pikey"}` : req.header["pikey"] != piPassword ? `password:${req.header["pikey"]}`:`username:${req.header["piname"]}`}`, "codeWarning");
+            res.statusCode(401).send("nope");
+        }
+    }
+});
+
 app.post("/info", async (req, res) => {
     try {
-        console.log(req.body);
-        if (req.body.type == 'lights') {
-            if(req.body.red >= 0 && req.body.red <= 255 &&
-                req.body.green >= 0 && req.body.green <= 255 &&
-                req.body.blue >= 0 && req.body.blue <= 255){
-            await doQuery(`update lights set lightValue = ${req.body.red} where lightColor = "red"`)
-            await doQuery(`update lights set lightValue = ${req.body.green} where lightColor = "green"`)
-            await doQuery(`update lights set lightValue = ${req.body.blue} where lightColor = "blue"`)
-            await doQuery('SELECT * FROM lights').then(results => {
-                console.log(results);
-                const sqlLogMes = `updated lights: ` +
-                    `${results[0][Object.keys(results[0])[1]]}:${results[0][Object.keys(results[0])[2]]} ` +
-                    `${results[1][Object.keys(results[1])[1]]}:${results[1][Object.keys(results[1])[2]]}`   +
-                    `${results[2][Object.keys(results[2])[1]]}:${results[2][Object.keys(results[2])[2]]} `
-                logEvent(sqlLogMes, "/dataBase");
-            })
-            }else{
-                errorLog("the user put in a bad request as: " + req.body);
-            }
-        } else if (req.body.type == 'drone') {
-            await doQuery('SELECT DroneOn FROM drone').then(async (val) => {
-                if (val[0][Object.keys(val[0])[0]] != req.body.droneOn) {
-                    console.log("working");
-                    await doQuery(`UPDATE drone SET DroneOn = ${req.body.droneOn} where id = 1`)
-                    await doQuery('SELECT * FROM drone').then(results => {
-                        const sqlLogMes = `updated drone` +
-                            `drone ` + `${results[0][Object.keys(results[0])[0]]}: ` + `${results[0][Object.keys(results[0])[1]]}`
-                        logEvent(sqlLogMes, "/dataBase")
-
+        if (userIps.indexOf(req.ip) == -1) {
+            console.log(req.ip);
+            userIps.push(req.ip);
+            console.log(req.body);
+            if (req.body.type == 'lights') {
+                if (req.body.red >= 0 && req.body.red <= 255 &&
+                    req.body.green >= 0 && req.body.green <= 255 &&
+                    req.body.blue >= 0 && req.body.blue <= 255) {
+                    await doQuery(`update lights set lightValue = ${req.body.red} where lightColor = "red"`)
+                    await doQuery(`update lights set lightValue = ${req.body.green} where lightColor = "green"`)
+                    await doQuery(`update lights set lightValue = ${req.body.blue} where lightColor = "blue"`)
+                    await doQuery('SELECT * FROM lights').then(results => {
+                        console.log(results);
+                        const sqlLogMes = `updated lights: ` +
+                            `${results[0][Object.keys(results[0])[1]]}:${results[0][Object.keys(results[0])[2]]} ` +
+                            `${results[1][Object.keys(results[1])[1]]}:${results[1][Object.keys(results[1])[2]]}` +
+                            `${results[2][Object.keys(results[2])[1]]}:${results[2][Object.keys(results[2])[2]]} `
+                        logEvent(sqlLogMes, "/dataBase");
                     })
-                    if (req.body.droneOn == 1) {
-                        setTimeout(async () => {
-                            console.log("stopping")
-                            await doQuery(`UPDATE drone SET droneOn = 0 where id = 1`)
-                            await doQuery('SELECT * FROM drone').then(results => {
-                                const sqlLogMes = `updated drone` +
-                                    `drone ` + `${results[0][Object.keys(results[0])[0]]}: ` + `${results[0][Object.keys(results[0])[1]]}`
-                                logEvent(sqlLogMes, "/dataBase");
-
-                            })
-                        },10000)
-                    }
+                } else {
+                    errorLog("the user put in a bad request as: " + req.body);
                 }
-            })
+            } else if (req.body.type == 'drone') {
+                await doQuery('SELECT DroneOn FROM drone').then(async (val) => {
+                    if (val[0][Object.keys(val[0])[0]] != req.body.droneOn) {
+                        console.log("working");
+                        await doQuery(`UPDATE drone SET DroneOn = ${req.body.droneOn} where id = 1`)
+                        await doQuery('SELECT * FROM drone').then(results => {
+                            const sqlLogMes = `updated drone` +
+                                `drone ` + `${results[0][Object.keys(results[0])[0]]}: ` + `${results[0][Object.keys(results[0])[1]]}`
+                            logEvent(sqlLogMes, "/dataBase")
+
+                        })
+                        if (req.body.droneOn == 1) {
+                            setTimeout(async () => {
+                                console.log("stopping")
+                                await doQuery(`UPDATE drone SET droneOn = 0 where id = 1`)
+                                await doQuery('SELECT * FROM drone').then(results => {
+                                    const sqlLogMes = `updated drone` +
+                                        `drone ` + `${results[0][Object.keys(results[0])[0]]}: ` + `${results[0][Object.keys(results[0])[1]]}`
+                                    logEvent(sqlLogMes, "/dataBase");
+
+                                })
+                            }, 5000)
+                        }
+                    }
+                })
+            }
+            setTimeout(async () => {
+                console.log("removing ip: " + userIps[0]);
+                userIps.shift();
+            },spamTime);
+        }else{
+            console.log(userIps);
+            console.log("your ip of " + req.ip + "has done a reqest already");
         }
     } catch (err) {
         console.log(err);
         errorLog(err);
         // res.sendStatus(404);
     }
+
     res.sendStatus(204);
 });
 
