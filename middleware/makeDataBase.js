61cf0ba2c1ec7eb1006606b300a839cdc6efc1f3
poosh316 <colsonmerrett@gmail.com>
2026-06-11 13:04:06 -0400

    fixed errors when databse is not loaded

--- middleware/makeDataBase.js
+++ middleware/makeDataBase.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: "10.191.164.205",
+    host: "10.191.164.55",
     user: "UnpaidIndieDev",
     password: "TheUnpaid123",
     database: "boardStatus",
