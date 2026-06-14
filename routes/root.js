8f1b6e43173072baa997c2d31fbcd460c5e1b4e6
StrawberrySugars <jasmine.soetemans@gmail.com>
2026-06-13 21:53:05 -0400

    uncomment temporary ddelete link

--- routes/root.js
+++ routes/root.js
@@ -18,21 +18,21 @@ router.get(/^\/signup(\/)?(.html)?$/, (req, res) => {
     res.sendFile(path.join(__dirname, "..", "views", "signup.html"));
 });
 
-//temporary delete link
-// router.get(/^\/del(\/)?(.html)?$/, (req, res) => {
-//     const token = req.cookies.int_token; //
+// temporary delete link
+router.get(/^\/del(\/)?(.html)?$/, (req, res) => {
+    const token = req.cookies.int_token; //
 
-//     if (token) {
-//         res.clearCookie('int_token', { //
-//             httpOnly: true,
-//             secure: true,
-//             sameSite: 'strict'
-//         });
-//         console.log("session deleted");
-//         res.redirect("/");
-//     } else {
-//         res.redirect('error.html');
-//     }
-// });
+    if (token) {
+        res.clearCookie('int_token', { //
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict'
+        });
+        console.log("session deleted");
+        res.redirect("/");
+    } else {
+        res.redirect('error.html');
+    }
+});
 
 module.exports = router;
